7f7dc56311b428351db63e1cd8ce219d3e38463a
Raj Natarajan <91092812+raj-natarajan-rnd@users.noreply.github.com>
2025-09-15 11:57:46 -0400

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,6 +1,11 @@
-\d acme.audit_log  -- psql
--- or
-SELECT table_schema, table_name, privilege_type
-FROM information_schema.table_privileges
-WHERE table_schema='acme' AND table_name='audit_log';
+-- Is RLS enabled/forced?
+SELECT c.relname, c.relrowsecurity, c.relforcerowsecurity
+FROM pg_class c
+WHERE c.oid = 'acme.audit_log'::regclass;
 
+-- Any policies?
+SELECT * FROM pg_policies
+WHERE schemaname='acme' AND tablename='audit_log';
+
+SHOW session_replication_role;  -- must be 'origin'
+SET SESSION session_replication_role = origin;
